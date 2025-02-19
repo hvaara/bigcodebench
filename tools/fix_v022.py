1197e6be4cd5a686cc4f6fb43ff74f27f6e51268
Roy Hvaara <roy@lightyear.no>
2025-02-19 02:11:56 +0100

    wip

--- tools/fix_v022.py
+++ tools/fix_v022.py
@@ -52,32 +52,32 @@ if __name__ == "__main__":
     ds = ds_dict[BIGCODEBENCH_VERSION]
     hard_ds = hard_ds_dict[BIGCODEBENCH_VERSION]
     # function_id = [211, 215, 1005]
-    function_id = [211, 215, 1005]
+    function_id = [211, 215]
     
     new_ds = ds.map(map_ds)
     new_ds.to_json("BigCodeBench.jsonl")
     ds_dict[BIGCODEBENCH_NEW_VERSION] = new_ds
-    ds_dict.push_to_hub(BIGCODEBENCH_HF)
+    # ds_dict.push_to_hub(BIGCODEBENCH_HF)
     
     new_hard_ds = hard_ds.map(map_ds)
     new_hard_ds.to_json("BigCodeBench-Hard.jsonl")
     hard_ds_dict[BIGCODEBENCH_NEW_VERSION] = new_hard_ds
-    hard_ds_dict.push_to_hub(BIGCODEBENCH_HARD_HF)
+    # hard_ds_dict.push_to_hub(BIGCODEBENCH_HARD_HF)
     
     for i in function_id:
         old_sample = ds.select([i])
         new_sample = new_ds.select([i])
         old_sample.to_json("old.jsonl")
         new_sample.to_json("new.jsonl")
-        api.upload_file(
-            path_or_fileobj="old.jsonl",
-            path_in_repo=f"{i}/old.jsonl",
-            repo_id=BIGCODEBENCH_UPDATE,
-            # repo_type="dataset"
-        )
-        api.upload_file(
-            path_or_fileobj="new.jsonl",
-            path_in_repo=f"{i}/new.jsonl",
-            repo_id=BIGCODEBENCH_UPDATE,
-            # repo_type="dataset"
-        )
+        # api.upload_file(
+        #     path_or_fileobj="old.jsonl",
+        #     path_in_repo=f"{i}/old.jsonl",
+        #     repo_id=BIGCODEBENCH_UPDATE,
+        #     # repo_type="dataset"
+        # )
+        # api.upload_file(
+        #     path_or_fileobj="new.jsonl",
+        #     path_in_repo=f"{i}/new.jsonl",
+        #     repo_id=BIGCODEBENCH_UPDATE,
+        #     # repo_type="dataset"
+        # )
